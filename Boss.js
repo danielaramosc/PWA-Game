28603f56a9a96793830fc2ad324f64ba55ad2353
danielaramosc <113630731+danielaramosc@users.noreply.github.com>
2024-12-22 19:25:19 +0100

    Delete Boss.js

--- Boss.js
+++
@@ -1,9 +0,0 @@
-// Boss.js
-class Boss extends Opponent {
-    constructor(game) {
-        super(game);
-        this.image.src = 'assets/boss.png';
-        this.myImageDead = 'assets/boss_dead.png';
-        this.speed *= 2;  
-    }
-}
